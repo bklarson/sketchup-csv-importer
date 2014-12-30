ac3cf0974761cf491e9327f658c11d94e31f4323
Brad Larson <bklarson@gmail.com>
2014-12-30 16:56:15 -0600

    Add text for created components
    This text will be very helpful when printing out the components as a guide to cut on the saw.

--- csv_importer.rb
+++ csv_importer.rb
@@ -26,12 +26,17 @@ def plywood_cuts
 	points[1] = [width, 0, 0]
 	points[2] = [width, length, 0]
 	points[3] = [0, length, 0]
-	new_face = new_comp_def.entities.add_face(points)
+	new_comp_def.entities.add_face(points)
+	# need to use a height of 1 here, otherwise the text is partway under
+	# the face and gets hidden (Unless it is moved in the Sketchup app, which
+	# seems to fix that.  Bug in the API?)
+	point = Geom::Point3d.new( width/2, length/2, 1 )
+	new_comp_def.entities.add_text( name, point )
 
 	for x in 1..number
-		point = Geom::Point3d.new 0, (x-1)*(length+1),0
-		trans = Geom::Transformation.new point
-		Sketchup.active_model.active_entities.add_instance(new_comp_def,trans);
+		point = Geom::Point3d.new( 0, (x-1)*(length+1), 0 )
+		trans = Geom::Transformation.new( point )
+		Sketchup.active_model.active_entities.add_instance( new_comp_def, trans )
 	end
 end
 

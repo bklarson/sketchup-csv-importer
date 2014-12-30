0d9d6a400bc6066c4929c3021e9ff0a7f30b0172
Brad Larson <bklarson@gmail.com>
2014-12-30 14:42:18 -0600

    Use same component multiple times
    When creating multiple copies, just reuse the same component.  This way, if the component needs to be changed for some reason, the change is applied to all copies.
    
    Also cleaned up the code spacing a bit.

--- csv_importer.rb
+++ csv_importer.rb
@@ -1,43 +1,37 @@
 require 'sketchup.rb'
 
-SKETCHUP_CONSOLE.show
+# SKETCHUP_CONSOLE.show
 
 UI.menu("Plugins").add_item("Plywood Cuts") {
-	UI.messagebox("I'm about to Cut some Wood!")
 	plywood_cuts
 }
 
 def plywood_cuts
- model = Sketchup.active_model
- entities = model.entities
+	model = Sketchup.active_model
+	entities = model.entities
 
- prompts = ["Name", "Number", "Length", "Width"]
- defaults = ["", "1", "6", "9"]
- inputs = UI.inputbox(prompts, defaults, "Give me some dimensions!")
- 
- name = inputs[0]
- number = inputs[1].to_f
- length = inputs[2].to_f
- width = inputs[3].to_f
- 
- for x in 1..number
- 	x1 = 0
-	x2 = width
-	y1 = (x - 1) * (length + 1)
-	y2 = y1 + length
-	
+	prompts = ["Name", "Number", "Length", "Width"]
+	defaults = ["", "1", "6", "9"]
+	inputs = UI.inputbox(prompts, defaults, "Give me some dimensions!")
+
+	name = inputs[0]
+	number = inputs[1].to_f
+	length = inputs[2].to_f
+	width = inputs[3].to_f
+
 	new_comp_def = Sketchup.active_model.definitions.add(name)
 
 	points = Array.new
-	points[0] = [x1, y1, 0]
-	points[1] = [x2, y1, 0]
-	points[2] = [x2, y2, 0]
-	points[3] = [x1, y2, 0]
-
+	points[0] = [0, 0, 0]
+	points[1] = [width, 0, 0]
+	points[2] = [width, length, 0]
+	points[3] = [0, length, 0]
 	new_face = new_comp_def.entities.add_face(points)
-	trans = Geom::Transformation.new
-	Sketchup.active_model.active_entities.add_instance(new_comp_def,trans);
 
- end
+	for x in 1..number
+		point = Geom::Point3d.new 0, (x-1)*(length+1),0
+		trans = Geom::Transformation.new point
+		Sketchup.active_model.active_entities.add_instance(new_comp_def,trans);
+	end
 end
 
